3b7f4ed69b1aca6daa90dec3ff73e8b994f74595
Peter van Zetten <peter.van.zetten@cgi.com>
2024-05-10 13:39:49 +0100

    feat: broken email receiving rule

--- lib/repro-ses-s3-rule-stack.ts
+++ lib/repro-ses-s3-rule-stack.ts
@@ -1,16 +1,46 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Construct} from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as ses from 'aws-cdk-lib/aws-ses';
+import * as actions from 'aws-cdk-lib/aws-ses-actions';
+
+export interface ReproSesS3RuleStackProps extends cdk.StackProps {
+  recipient: string;
+}
 
 export class ReproSesS3RuleStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: ReproSesS3RuleStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const emailStoreBucket = new s3.Bucket(this, 'TestEmailStore', {
+      accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      versioned: true,
+      enforceSSL: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      lifecycleRules: [
+        {
+          id: 'DeleteEmailsAfter30Days',
+          prefix: 'Email/',
+          expiration: cdk.Duration.days(30),
+        },
+      ],
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ReproSesS3RuleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const receiptRuleSet = ses.ReceiptRuleSet.fromReceiptRuleSetName(this, 'TestRuleSet', 'TestRuleSet');
+
+    receiptRuleSet.addRule('StoreToBucketRule', {
+      receiptRuleName: 'StoreToBucketRule',
+      recipients: [props.recipient],
+      actions: [
+        new actions.S3({
+          bucket: emailStoreBucket,
+          objectKeyPrefix: 'emails/',
+        }),
+      ],
+      enabled: true,
+    });
   }
 }
