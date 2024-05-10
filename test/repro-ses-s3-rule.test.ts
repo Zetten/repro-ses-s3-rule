43d2bbc33b0622ecb6cde90651f7f2582c6d7fe1
Peter van Zetten <peter.van.zetten@cgi.com>
2024-05-10 11:51:03 +0100

    Initial commit

---
+++ test/repro-ses-s3-rule.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ReproSesS3Rule from '../lib/repro-ses-s3-rule-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/repro-ses-s3-rule-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ReproSesS3Rule.ReproSesS3RuleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
